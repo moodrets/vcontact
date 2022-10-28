dd70a9221dbfa74f345eb95fa40866ac8a32cd1e
moodrets <retsdmitriy@gmail.com>
2022-10-28 14:09:21 +0500

    new deploy changes

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name].[hash].js',
 		clean: true,
-		publicPath: '/',
+		publicPath: '/vcontact/',
 	},
 	resolve: {
 		extensions: ['*', '.js', '.ts', '.vue', 'scss', 'css'],
